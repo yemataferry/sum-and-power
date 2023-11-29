321f66230d3209175a9e473fd3f884bc649dfe3f
Yemata Ferry <yemataferry@gmail.com>
2023-11-29 09:38:09 +0100

    power function done

--- assets/scripts/index1.js
+++ assets/scripts/index1.js
@@ -1,5 +1,10 @@
 function pow (x, y) {
-  return x^y
+  let sum = 1;
+  for (let i = 1; i<=y; i++){
+    sum *= x;
+  }
+  return (sum)
 }
 
-console.log(pow(4, 2))
+console.log(pow(4, 2));
+console.log(pow(5, 3));
